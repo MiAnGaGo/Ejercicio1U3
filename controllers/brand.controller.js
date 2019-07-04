c2d6019963b29a8e4cd047e7177bf33299398958
MiguelGarcia <miangarciago@ittepic.edu.mx>
2019-07-04 09:52:26 -0600

    primer commit crud usuarios sin update

---
+++ controllers/brand.controller.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const path = require('path');
+const status = require('http-status');
+
+let _brand;
+
+
+
+module.exports = (Brand) => {
+    _brand = Brand;
+    return({
+        
+    });
+}
---
+++ routers/brand.router.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+module.exports = (wagner) => {
+    
+    const brandCtrl = wagner.invoke((Brand) => 
+        require('../controllers/brand.controller')(Brand));
+
+    
+    return router;
+}
